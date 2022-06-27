0a5f7e35fd8d884eb7446362c4d843f102aa2e73
Quang Vinh <quangvinhkhongcovo@gmail.com>
2022-06-27 13:18:47 +0700

    config GoogleSignInAccount for check exist sign-in user

--- app/src/main/java/com/example/backup_restore/MainActivity.java
+++ app/src/main/java/com/example/backup_restore/MainActivity.java
@@ -3,14 +3,17 @@ package com.example.backup_restore;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 
 public class MainActivity extends AppCompatActivity {
     GoogleSignInOptions gso;
     GoogleSignInClient mGoogleSignInClient;
+    GoogleSignInAccount gsa;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +30,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
+        gsa= GoogleSignIn.getLastSignedInAccount(this);
+        updateUI(gsa);
+    }
 
+    private void updateUI(GoogleSignInAccount gsa) {
+        Log.d("BBB", "updateUI: " + gsa.toString());
     }
 }
