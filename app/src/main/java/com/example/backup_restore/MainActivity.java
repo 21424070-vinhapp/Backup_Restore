af3859fb411834ac2dd4ad7895a219ee618f9120
Quang Vinh <quangvinhkhongcovo@gmail.com>
2022-06-27 13:13:38 +0700

    config GoogleSignInClient project & GoogleSignInOption

--- app/src/main/java/com/example/backup_restore/MainActivity.java
+++ app/src/main/java/com/example/backup_restore/MainActivity.java
@@ -4,11 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 
+public class MainActivity extends AppCompatActivity {
+    GoogleSignInOptions gso;
+    GoogleSignInClient mGoogleSignInClient;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        gso=new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient= GoogleSignIn.getClient(this,gso);
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
     }
 }
