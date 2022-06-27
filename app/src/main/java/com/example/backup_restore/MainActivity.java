24bd93d0e8f3e0059999c44beab42b0bf37cc88d
Quang Vinh <quangvinhkhongcovo@gmail.com>
2022-06-27 14:51:27 +0700

    finish sign-in google

--- app/src/main/java/com/example/backup_restore/MainActivity.java
+++ app/src/main/java/com/example/backup_restore/MainActivity.java
@@ -1,40 +1,96 @@
 package com.example.backup_restore;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
+import com.example.backup_restore.databinding.ActivityMainBinding;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int RC_SIGN_IN = 1000;
     GoogleSignInOptions gso;
     GoogleSignInClient mGoogleSignInClient;
     GoogleSignInAccount gsa;
+    ActivityMainBinding mainBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        mainBinding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mainBinding.getRoot());
+
+        // Set the dimensions of the sign-in button
+        mainBinding.signInButton.setSize(SignInButton.SIZE_STANDARD);
 
         gso=new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
                 .build();
 
         mGoogleSignInClient= GoogleSignIn.getClient(this,gso);
+
+        //event button sign-in
+        event();
     }
 
+    private void event() {
+        mainBinding.signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signIn();
+            }
+        });
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+
+    private void handleSignInResult(Task<GoogleSignInAccount> task) {
+        try {
+            GoogleSignInAccount account=task.getResult(ApiException.class);
+            Intent intent=new Intent(MainActivity.this,MainActivity2.class);
+            startActivity(intent);
+        } catch (ApiException e) {
+            e.printStackTrace();
+            Log.w("BBB", "signInResult:failed code=" + e.getStatusCode());
+        }
+    }
 
     @Override
     protected void onStart() {
         super.onStart();
         gsa= GoogleSignIn.getLastSignedInAccount(this);
-        updateUI(gsa);
     }
 
-    private void updateUI(GoogleSignInAccount gsa) {
-        Log.d("BBB", "updateUI: Access");
-    }
 }
---
+++ app/src/main/java/com/example/backup_restore/MainActivity2.java
@@ -0,0 +1,33 @@
+package com.example.backup_restore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+
+import java.util.List;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
+        if (acct != null) {
+            String personName = acct.getDisplayName();
+            String personGivenName = acct.getGivenName();
+            String personFamilyName = acct.getFamilyName();
+            String personEmail = acct.getEmail();
+            String personId = acct.getId();
+            Uri personPhoto = acct.getPhotoUrl();
+
+            Log.d("BBB", "onCreate: " + personEmail);
+        }
+    }
+}
